d10fa49a36972d095f97440d3ca9ca1f03037100
machine <28401480+machine62@users.noreply.github.com>
2018-04-02 18:33:48 +0200

    Premier commit
    ajout squelette

---
+++ install.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!defined('IN_SPYOGAME'))
+    die("Hacking attempt");
+
+$security = false;
+$mod_folder = "api";
+$security = install_mod($mod_folder);
+if ($security == true) {
+    // nothing
+	
+	
+	
+   
+}
+
+
+
---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+
+
+if (!defined('IN_SPYOGAME')) die("Hacking attempt");
+
+$mod_uninstall_name = "api";
+
+
+uninstall_mod($mod_uninstall_name,null);
+
+ 
+?>
