9f79b445c1df07942e01b65d869a1b5235d40ecb
machine62 <28401480+machine62@users.noreply.github.com>
2023-04-24 14:33:51 +0200

    liste des fonctions
    Affichage moche ... sera a revoir

--- index.php
+++ index.php
@@ -1,12 +1,63 @@
 <?php
-if (!defined('IN_SPYOGAME')) die("Hacking attempt"); // Pas d'accès direct
+if (!defined('IN_SPYOGAME'))
+    die("Hacking attempt"); // Pas d'accès direct
 require_once("views/page_header.php");
 
-
+require_once("mod/api/core/webApi.php");
+$def = webApi::getDefinition();
 ?>
 
-<h2>API Ogspy</h2>
-<p>A suivre ....</p>
+
+<h2>Liste des appels</h2>
+
+<table>
+
+    <tbody>
+        <?php foreach ($def as $ressource => $definitions) : ?>
+            <tr> 
+                <td class="c" colspan="7"><?php echo $ressource; ?></td>
+            </tr>
+            <?php if (isset($definitions["description"])) : ?>
+                <tr>
+                    <td colspan="7"><?php echo $definitions["description"]; ?></td>
+                </tr>
+            <?php endif; ?>
+
+            <?php if (isset($definitions["arguments"]) && count($definitions["arguments"]) > 0) : ?>
+                <tr>
+                    <td></td>
+                    <th>Argument</th>
+                    <th>cast</th>
+                    <th>required</th>
+                    <th>min</th>
+                    <th>max</th>
+                    <th>description</th>
+                </tr>
+
+                <?php foreach ($definitions["arguments"] as $key => $value) : ?>
+                    <tr>
+                        <td> </td>
+                        <td><?php echo $key; ?> </td>
+                        <td><?php echo $value["cast"]; ?> </td>
+                        <td><?php echo $value["required"] ? 'Oui' : 'Non';  ?> </td>
+                        <td><?php echo $value["min"]; ?> </td>
+                        <td><?php echo $value["max"]; ?> </td>
+                        <td><?php echo $value["description"]; ?> </td>.
+                    </tr> 
+                <?php endforeach; ?>
+
+            <?php endif; ?>
+
+        <?php endforeach; ?>
+
+
+
+
+    </tbody>
+
+
+</table>        
+
 
 
 
